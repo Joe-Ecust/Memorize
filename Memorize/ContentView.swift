b8b8ed572289b0eed07fe8a871f16370c8d7e3e2
aiselfi <zhou.jiale@ecust.edu.cn>
2024-02-21 17:38:03 +0800

    new feature: cardadjusters

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,41 +9,71 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻","🎃","😈","🕷️", "🌈", "🌹","🌧️","🏓","🚘","🚄","⛽️"]
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack{
-            CardView(isFaceUp: true);
-            CardView();
-            CardView();
-            CardView();
+        VStack{
+            cards
+            cardCountAdjusters
         }
         .foregroundColor(.orange)
         .padding()
     }
+    
+    var cards: some View{
+        HStack{
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index]);
+            }
+        }
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            Button(action: {
+                if(cardCount > 1){
+                    cardCount -= 1
+                }
+            }, label: {
+                Image(systemName: "rectangle.stack.badge.minus.fill")
+            })
+            Spacer()
+            
+            Button("Reset Card") {
+                cardCount = 4
+            }
+            Spacer()
+            Button("Add Card") {
+                if(cardCount < emojis.count){
+                    cardCount += 1
+                }
+            }
+        }
+        //            .imageScale(.large)
+        //            .font(.largeTitle)
+    }
 }
 
 struct CardView: View {
-    @State var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack{
             let base = RoundedRectangle(
                 cornerRadius: 12
             )
-            var x = 1
             
             if(isFaceUp){
-                base.fill(
-                    .white
-                )
+                base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
         }
         .onTapGesture {
-            print("tapped")
             isFaceUp.toggle()
         }
     }
